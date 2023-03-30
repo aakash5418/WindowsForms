5364c6608ef74b033d40ed145defe17c24c128ed
Aakash <95157200+aakash5418@users.noreply.github.com>
2023-03-30 12:19:39 +0530

    Add files via upload

---
+++ Tickects/Tickects/FrmTickets.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Tickects
+{
+    partial class FrmTickets
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTickects = new System.Windows.Forms.Button();
+            this.btnGas = new System.Windows.Forms.Button();
+            this.btnHydro = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTickects
+            // 
+            this.btnTickects.Location = new System.Drawing.Point(237, 97);
+            this.btnTickects.Name = "btnTickects";
+            this.btnTickects.Size = new System.Drawing.Size(280, 48);
+            this.btnTickects.TabIndex = 0;
+            this.btnTickects.Text = "DetermineTickets";
+            this.btnTickects.UseVisualStyleBackColor = true;
+            this.btnTickects.Click += new System.EventHandler(this.btnTickects_Click);
+            // 
+            // btnGas
+            // 
+            this.btnGas.Location = new System.Drawing.Point(237, 233);
+            this.btnGas.Name = "btnGas";
+            this.btnGas.Size = new System.Drawing.Size(280, 47);
+            this.btnGas.TabIndex = 1;
+            this.btnGas.Text = "Gas";
+            this.btnGas.UseVisualStyleBackColor = true;
+            this.btnGas.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnHydro
+            // 
+            this.btnHydro.Location = new System.Drawing.Point(237, 362);
+            this.btnHydro.Name = "btnHydro";
+            this.btnHydro.Size = new System.Drawing.Size(280, 47);
+            this.btnHydro.TabIndex = 2;
+            this.btnHydro.Text = "HydroGas";
+            this.btnHydro.UseVisualStyleBackColor = true;
+            this.btnHydro.Click += new System.EventHandler(this.btnHydro_Click);
+            // 
+            // FrmTickets
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnHydro);
+            this.Controls.Add(this.btnGas);
+            this.Controls.Add(this.btnTickects);
+            this.Name = "FrmTickets";
+            this.Text = "Tickets";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTickects;
+        private System.Windows.Forms.Button btnGas;
+        private System.Windows.Forms.Button btnHydro;
+    }
+}
+
---
+++ Tickects/Tickects/FrmTickets.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tickects
+{
+    public partial class FrmTickets : Form
+    {
+        public FrmTickets()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTickects_Click(object sender, EventArgs e)
+        {
+            int age;
+            double price = 0;
+
+            age = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("please enter your age:", "Tickects", ""));
+
+            if (age <= 10)
+            {
+                price = 5.0;
+            }
+            else if (age >= 11 && age <= 18 )
+            {
+                price = 10.0;
+
+            }
+            else if (age >= 19 && age <= 45 )
+            {
+                price = 20.0;
+            }
+            else if (age >= 46 )
+            {
+                price = 5.5;
+            }
+
+            MessageBox.Show("Your price is :" + price.ToString("c"));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int gas;
+            double bill = 0;
+
+            gas = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("please enter your gas useage in ccf:", "Gas Bill", ""));
+
+            if (gas <= 10)
+            {
+                bill = gas * .50;
+            }
+            else if (gas >= 11 )
+            {
+                bill = 11 * .50 + (gas - 11) * .30;
+            }
+            MessageBox.Show ("Your gas bill is" + bill.ToString("c"));
+        }
+
+        private void btnHydro_Click(object sender, EventArgs e)
+        {
+            int kwh;
+            double bill = 0;
+
+            kwh = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("please enter your kwh usage:", "Hydro Bill", ""));
+             
+            if (kwh <= 12)
+            {
+                bill = 2.80;
+            }
+            else if (kwh >12 && kwh >90)
+            {
+                bill = 2.80 + (kwh - 12) * 0.8;
+            }
+            else if (kwh <90)
+            {
+                bill = 2.80 + 78 * 0.8 + (kwh - 90) * 0.6;
+            }
+
+            MessageBox.Show("Your hydrogas bill is " + bill.ToString("c"));
+        }
+    }
+}
